67a02d2a295c46bd57aa464183c0dca4a05d3e9c
Leroy Leslie <leroy@Leroys-MacBook-Pro.local>
2017-01-15 18:06:22 -0500

    commented out script to help box run

--- app/javascripts/object/prefabs/Example.js
+++ app/javascripts/object/prefabs/Example.js
@@ -1,14 +1,14 @@
+/** UNCOMMENT TOP
 // Import other prefabs here
 import Player from "./Player.js";
 
-// Create your class here.  Extend from Phaser.ClassName - look at 2.6.1 docs 
+// Create your class here.  Extend from Phaser.ClassName - look at 2.6.1 docs
 class Example extends Phaser.ClassName {
 
-  /**
-      Constructor is your own constructor
-      Place in parameters needed for super
-      Also place additional params needed to make this work
-  */
+      // Constructor is your own constructor
+      // Place in parameters needed for super
+      // Also place additional params needed to make this work
+
   constructor({ param_1, param_2, ... , param_n }){
 
     // super is specific to whatever you are extending from
@@ -20,13 +20,9 @@ class Example extends Phaser.ClassName {
     console.log('Example working');
   };
 
-  /**
-    initial functions here!
-  */
+    // initial functions here!
 
-  /**
-    assess functions here!
-  */
+    // assess functions here!
 
   // Have an update function for each prefab!
   update() {}
@@ -34,3 +30,4 @@ class Example extends Phaser.ClassName {
 
 // Always export your class at the end!
 export default Example;
+*/
